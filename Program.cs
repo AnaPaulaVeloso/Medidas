17bbbbff2a0fc1d39ea55652aa0eedf5bd20a3ba
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-09-17 20:48:50 -0300

    Concluir a tarefa Medidas

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double metros, metrosCm, metrosKm;
+            Console.Write("Entre com a medida em (m):");
+            metros = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEquivalência");
+
+            metrosCm = metros*100;
+
+            Console.WriteLine($"{metrosCm} cm");
+            Console.WriteLine($"{metros} m");
+
+            metrosKm = metros/1000;
+
+            Console.WriteLine($"{metrosKm} Km");
+
+
+        }
+    }
+}
